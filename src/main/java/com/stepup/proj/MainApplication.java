9529189d1ea2e5ec68286d34e44de0628c6d3fd4
Dzhamilya Iskhakova <jemmswsw@gmail.com>
2025-08-30 18:19:52 +0300

    улучшили логику main и doSomething

--- src/main/java/com/stepup/proj/MainApplication.java
+++ src/main/java/com/stepup/proj/MainApplication.java
@@ -4,13 +4,14 @@ public class MainApplication {
     public static void main(String[] args) {
         System.out.println("Hello World!");
         System.out.println("1001");
+        System.out.println("---");
     }
 }
 
 public class MainApplication {
     public static void main(String[] args) {
         System.out.println("1");
-        System.out.println("2");
-        System.out.println("3");
+        System.out.println("0");
+        System.out.println("0");
     }
 }
